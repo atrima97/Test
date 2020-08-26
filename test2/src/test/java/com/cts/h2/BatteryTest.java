565c9bccea655c3a251538c85a45ac15adfb1892
atrima97 <69793856+atrima97@users.noreply.github.com>
2020-08-26 17:20:08 +0530

    Add files via upload

---
+++ test2/src/test/java/com/cts/h2/BatteryTest.java
@@ -0,0 +1,14 @@
+package com.cts.h2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BatteryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
